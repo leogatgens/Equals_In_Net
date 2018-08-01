c146754cc878266f36facc66d497caf86ab68581
Leonardo Meléndez <leogatgens@gmail.com>
2018-08-01 15:39:45 -0600

    Create project to test equals in .NET

---
+++ EqualsAlgoritms/EqualsAlgoritms.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EqualsAlgoritms.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EqualsAlgoritms.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ac81600-a126-477e-ba77-9f9ec2f355be")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EqualsAlgoritms/EqualsAlgoritms/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EqualsAlgoritms
+{
+    public class Class1
+    {
+    }
+}
